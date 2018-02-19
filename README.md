610dcec799fcf04cce270cdfe378e862ba3391d0
Spencer Kent <spencer.kent@berkeley.edu>
2018-02-19 12:40:59 -0800

    first commit. Basic file structure

---
+++ README.md
@@ -0,0 +1,7 @@
+gdn-autoencoder
+======
+My first attempt at training an autoencoder with GDN nonlinearity to do 
+transform coding of images. 
+
+### Authors
+* Spencer Kent
---
+++ gdn_autoencoder/__init__.py
@@ -0,0 +1,7 @@
+"""
+This package attempts naively to implement an autoencoder with GDN nonlinearity
+"""
+
+__version__ = '0.1'
+__author__ = 'Spencer Kent'
+__email__ = 'spencer.kent@berkeley.edu'
