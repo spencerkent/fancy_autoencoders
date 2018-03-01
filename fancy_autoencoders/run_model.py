3bf1b7eab843efee526930155614204448a8870c
Spencer Kent <spencer.kent@berkeley.edu>
2018-02-28 19:05:39 -0800

    checkpoint for Dylan and Sophia to try some experiments

--- fancy_autoencoders/run_model.py
+++ fancy_autoencoders/run_model.py
@@ -5,68 +5,92 @@ import sys
 dsc_folder = '/home/spencerkent/Projects/DeepSparseCoding/'
 sys.path.insert(1, dsc_folder)
 
+import pickle
 import numpy as np
+from matplotlib import pyplot as plt
 
 from models import GDN_autoencoder
-from models import RELU_autoencoder
+# from models import RELU_autoencoder
+# from models import sigmoid_autoencoder
 import utils.plotting
 
-run_identifier = 'Hunting_for_filters_GDN_0_1s_15wd'
+run_identifier = 'testing_before_push'
+logfile_directory = '/media/expansion1/spencerkent/logfiles/fancy_autoencoder/'
 
 image_params = {'height': 16, 'width': 16, 'depth': 1}
-hidden_layer_params = {'weight_init_std': 0.1, 'code_size': 256}  # two times overcomplete
+hidden_layer_params = {'weight_init_std': 0.1, 'code_size': 256}
 output_layer_params = {'weight_init_std': 0.1,
-                       'loss_type': 'l2_recon_l1_sparsity',
-                       'init_sparsity_weight': 0.1,
-                       'init_weight_decay_weight': 0.15}
-optimization_step_schedule = {0: 0.001, 30000: 0.0005,
-                              50000: 0.0001, 150000: 0.00002}
-batch_size = 500
-
+                       'loss_type': 'l2recon_l1sparsity_wd'}
+optimization_step_schedule = {'OPT_step': {0: 0.01, 200000: 0.004,
+                                           500000: 0.001},
+                              'sparsity_wt': {0: 0.000},
+                              'weight_decay_wt': {0: 0.13}}
+batch_size = 256
 # #sophias dataset for cosyne
 training_set_filename = '/home/spencerkent/Datasets/vanHaterenNaturalScenes/sophias_patches_preZCAwhite_train.npz'
 testing_set_filename = '/home/spencerkent/Datasets/vanHaterenNaturalScenes/test_zca_wht_v2.npz'
 # baboon_filename = '/home/spencerkent/Datasets/vanHaterenNaturalScenes/baboon_zca_wht.npz'
-with np.load(training_set_filename, fix_imports=True) as d:
-  sanborn_dset = d['arr_0'].item()['train']
-with np.load(testing_set_filename, fix_imports=True) as d:
-  sanborn_dset_test = d['arr_0'].item()['train']
-# with np.load(baboon_filename, fix_imports=True) as d:
-#   baboon = d['arr_0'].item()['train']
 
-
-num_val = int(0.2 * sanborn_dset.num_examples)  # number of validation patches
-random_idx = np.arange(sanborn_dset.num_examples)
-np.random.shuffle(random_idx)
-train_dset = sanborn_dset.images[random_idx[0:-num_val], :]
-val_dset = sanborn_dset.images[random_idx[-num_val:], :]
-test_dset = np.copy(sanborn_dset_test.images)
-raw_component_means = np.mean(train_dset, axis=0)
-#^ save this to add back in later
-train_dset = train_dset - raw_component_means[None, :]
-val_dset = val_dset - raw_component_means[None, :]
-test_dset = test_dset - raw_component_means[None, :]
 
 
 def main():
+  model_params = {'image_params': image_params,
+                  'hidden_layer_params': hidden_layer_params,
+                  'output_layer_params': output_layer_params}
+  pickle.dump(model_params, open(logfile_directory + run_identifier +
+                                 '_model_params.p', 'wb'))
 
-  logfile_directory = '/media/expansion1/spencerkent/logfiles/fancy_autoencoder/'
+  # wrangling the natural image patches dataset
+  with np.load(training_set_filename, fix_imports=True) as d:
+    sanborn_dset = d['arr_0'].item()['train']
+  with np.load(testing_set_filename, fix_imports=True) as d:
+    sanborn_dset_test = d['arr_0'].item()['train']
+  # with np.load(baboon_filename, fix_imports=True) as d:
+  #   baboon = d['arr_0'].item()['train']
+
+  preproc_subtractive_term = np.min(sanborn_dset.images)
+  preproc_divisive_term = np.max(sanborn_dset.images -
+                                 preproc_subtractive_term)
+
+  num_val = int(0.2 * sanborn_dset.num_examples)  # number of validation patches
+  random_idx = np.arange(sanborn_dset.num_examples)
+  np.random.shuffle(random_idx)
+  train_dset = sanborn_dset.images[random_idx[0:-num_val], :]
+  train_dset = (train_dset - preproc_subtractive_term) / preproc_divisive_term
+  val_dset = sanborn_dset.images[random_idx[-num_val:], :]
+  val_dset = (val_dset - preproc_subtractive_term) / preproc_divisive_term
+  test_dset = np.copy(sanborn_dset_test.images)
+  test_dset = (test_dset - preproc_subtractive_term) / preproc_divisive_term
+
+  # now some mean subtraction
+  raw_component_means = np.mean(train_dset, axis=0)
+  train_dset = train_dset - raw_component_means
+  val_dset = val_dset - raw_component_means
+  test_dset = test_dset - raw_component_means
+  np.savez(open(
+    '/home/spencerkent/Datasets/vanHaterenNaturalScenes/preproc_params.npz',
+    'wb'), means=raw_component_means, preproc_div=preproc_divisive_term,
+           preproc_sub=preproc_subtractive_term)
+
+
+
 
   print("Constructing Tensorflow Graph")
-  gdn_autoencoder = GDN_autoencoder(hidden_layer_params, output_layer_params,
-                                    image_params, optimization_step_schedule[0])
+  sig_ae = GDN_autoencoder(hidden_layer_params, output_layer_params,
+                                    image_params)
 
-  tfsession = gdn_autoencoder.StartSession()
+  # training from scratch
+  tfsession = sig_ae.StartSession()
 
-  trn_report = gdn_autoencoder.Train(tfsession, train_dset, val_dset,
-                                     batch_size, optimization_step_schedule,
-                                     snapshot_progress=True,
-                                     logging_dir=logfile_directory,
-                                     report_every=1000, max_epochs=200)
-  gdn_autoencoder.SaveSession(tfsession, logfile_directory, run_identifier)
+  trn_report = sig_ae.Train(tfsession, train_dset, val_dset, batch_size,
+                            optimization_step_schedule, snapshot_progress=True,
+                            logging_dir=logfile_directory, report_every=1000,
+                            max_epochs=150)
+
+  sig_ae.SaveSession(tfsession, logfile_directory, run_identifier)
 
   # plot the learned encoding and decoding weights
-  learned_enc, learned_dec = gdn_autoencoder.GetEncDecWeights(tfsession)
+  learned_enc, learned_dec = sig_ae.GetEncDecWeights(tfsession)
   enc_figs = utils.plotting.plot_weights(learned_enc, image_params, 'encoding',
                                          single_img=True, renormalize=True)
   dec_figs = utils.plotting.plot_weights(learned_dec, image_params, 'decoding',
@@ -82,7 +106,7 @@ def main():
 
   some_rand_inds = np.random.choice(np.arange(val_dset.shape[0]), 9,
                                     replace=False)
-  coeffs = gdn_autoencoder.GetCodes(tfsession, val_dset)
+  coeffs = sig_ae.GetCodes(tfsession, val_dset)
   coeff_figs = utils.plotting.analyze_code(coeffs, some_rand_inds)
   coeff_figs[0].savefig(logfile_directory + run_identifier +
                         '_activation_hists.png')
@@ -90,7 +114,7 @@ def main():
   coeff_figs[1].savefig(logfile_directory + run_identifier +
                         '_random_samp_codes.png')
   plt.close(coeff_figs[1])
-  final_loss, final_recons = gdn_autoencoder.Test(tfsession, val_dset, True)
+  final_loss, final_recons = sig_ae.Test(tfsession, val_dset, True)
   recon_figs = utils.plotting.plot_reconstructions(val_dset, final_recons,
       image_params, some_rand_inds)
   recon_figs[0].savefig(logfile_directory + run_identifier +
